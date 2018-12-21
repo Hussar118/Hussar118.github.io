e2a1a69b73f3e82e3ea35377457e870b93d505c2
Hussar118 <1449598186@qq.com>
2018-12-21 16:56:32 +0800

    Site updated: 2018-12-21 16:56:32

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,17 +1,17 @@
  <!--浏览器搞笑标题-->
- var OriginTitle = document.title;
- var titleTime;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/fengye.png");
-         document.title = '  (\'°A°\')页面崩溃啦！';
-         clearTimeout(titleTime);
-     }
-     else {
-         $('[rel="icon"]').attr('href', "/img/fengye.png");
-         document.title = 'Hussar博客' + OriginTitle;
-         titleTime = setTimeout( () {
-             document.title = OriginTitlfunctione;
-         }, 2000);
-     }
- });
+    var OriginTitle = document.title;
+    var titleTime;
+    document.addEventListener('visibilitychange', function () {
+        if (document.hidden) {
+            $('[rel="icon"]').attr('href', "/img/fengye.png");
+            document.title = '  (\'°A°\')页面崩溃啦！';
+            clearTimeout(titleTime);
+        }
+        else {
+            $('[rel="icon"]').attr('href', "/img/fengye.png");
+            document.title = 'Hussar博客' + OriginTitle;
+            titleTime = setTimeout( () {
+                document.title = OriginTitlfunctione;
+            }, 2000);
+        }
+    });
