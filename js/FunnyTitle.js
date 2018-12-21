7006d0d94ed55cd722b060d1fcb595b63d49f2db
Hussar118 <1449598186@qq.com>
2018-12-21 14:30:12 +0800

    Site updated: 2018-12-21 14:30:11

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -10,8 +10,8 @@
      else {
          $('[rel="icon"]').attr('href', "/img/fengye.png");
          document.title = 'Hussar博客' + OriginTitle;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
+         titleTime = setTimeout( () {
+             document.title = OriginTitlfunctione;
          }, 2000);
      }
  });
